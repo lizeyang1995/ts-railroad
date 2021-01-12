982b31c517648580c6f4c3e89c54aedd4fb67778
lizeyang1995 <424011527@qq.com>
2021-01-12 10:36:11 +0800

    [lizeyang] feat: parsed the graph

---
+++ CalculateRouterDistnce/graph/index.ts
@@ -0,0 +1,17 @@
+export interface IGraphType {
+    startPoint: string,
+    endPoint: string,
+    distance: number
+}
+
+const graphsList = ["AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"]
+
+const parsedGraphs: IGraphType[] = graphsList.map((graph) => {
+    return {
+        startPoint: graph[0],
+        endPoint: graph[1],
+        distance: Number(graph[2])
+    }
+})
+
+export default parsedGraphs
